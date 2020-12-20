047d0349b212a2a9dd9b06ce9fbc7b6a91cdcb94
GMaxHTW <72798750+GMaxHTW@users.noreply.github.com>
2020-12-20 16:03:05 +0100

    Klasse DrinkValue Stellt relevante Produktdaten von Getränken im Konstruktor zur Verfügung

---
+++ src/SupermarketSystem/domain/DrinkValue.java
@@ -0,0 +1,18 @@
+package SupermarketSystem.domain;
+
+public class DrinkValue {
+
+    private double volume;
+    private double calorficValue;
+    private double carbohydrates;
+    private double sugar;
+    private double alcoholContent;
+
+    public DrinkValue(double volume, double calorficValue, double carbohydrates, double sugar, double alcoholContent) {
+        this.volume = volume;
+        this. calorficValue = calorficValue;
+        this.carbohydrates = carbohydrates;
+        this.sugar = sugar;
+        this.alcoholContent = alcoholContent;
+    }
+}
