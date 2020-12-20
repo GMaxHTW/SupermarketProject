a74b3cb39fe2448e979fc2c7daa426b5cb879fae
GMaxHTW <72798750+GMaxHTW@users.noreply.github.com>
2020-12-20 16:08:01 +0100

    1) Unnötige Kommentare gelöscht

--- src/SupermarketSystem/domain/DrugstoreArticle.java
+++ src/SupermarketSystem/domain/DrugstoreArticle.java
@@ -3,7 +3,6 @@ package SupermarketSystem.domain;
 public class DrugstoreArticle extends AbstractProduct{
 
 
-    // Konstruktor DrugstoreArticles
     public DrugstoreArticle(String name, double salesPrice, double purchasePrice) {
         super(name, salesPrice, purchasePrice);
     }
@@ -13,12 +12,4 @@ public class DrugstoreArticle extends AbstractProduct{
         return "Name des Produktes: " + getName() + "\nVerkaufspreis: " + getSalesPrice()
                 + "\nEinkaufspreis: " + getPurchasePrice();
     }
-
-//    @Override
-//    public int compareTo(Object o) {
-//        if (o instanceof AbstractProduct) {
-//
-//        }
-//        return 0;
-//    }
 }
